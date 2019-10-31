74d1fa713d815d035bad3b1fde66da1833425f5f
xiaomi <xiaomi@china.ch>
2019-10-31 22:12:51 +0300

    Fix create_node function
    Add byte-by-byte copying from value to v

--- node.h
+++ node.h
@@ -6,6 +6,13 @@ typedef struct node{
 node *create_node(node *next_node,const void *value, size_t size){
 	node *n = malloc(sizeof(node));
 	n->next_node = next_node;
-	n->value = value;
+	void *v = malloc(size);
+	
+	
+	for(size_t i = 0; i<size; i++){
+		*(char*)(v+i) = *(char*)(value+i);
+	}
+	
+	n->value = v;
 	return n;
 }
