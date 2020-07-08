3f7e3322469bd6e1c661b1a75b2f921dfc1a984f
xiaomi <xiaomi@china.ch>
2020-07-08 23:49:39 +0300

    Update usage example of crc32 functions

--- example.c
+++ example.c
@@ -7,19 +7,8 @@
 crc32_config crc32_ieee_8023 = {0x4C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, 1, 1};
 
 int main() {
-	
-    CRC32_CACHE cache;
-	errno = 0;
-    crc32_load_cache(crc32_ieee_8023, "crc32_byte_value_cache.bin", cache);
-	if (errno != 0){
-		char str[255];
-		sprintf(str, "%s:%d: error", __FILE__, __LINE__);
-		perror(str);
-		abort();
-	}
-	
     const char data[] = "123456789";
-    uint32_t crc_value = crc32_optimized(data, 9, crc32_ieee_8023, cache);
+    uint32_t crc_value = crc32_optimized(data, 9, crc32_ieee_8023);
     printf("0x%x\n", crc_value);
 	
 	crc_value = crc32(data, 9, crc32_ieee_8023);
